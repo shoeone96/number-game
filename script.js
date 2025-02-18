613a520950315ff826b9252fec5c13e6889c6eaa
LEE JUNGWON <85065626+shoeone96@users.noreply.github.com>
2025-02-18 20:31:21 +0900

    fix: reset 시 버그 fix

--- script.js
+++ script.js
@@ -22,16 +22,18 @@ resetButton.addEventListener('click', reset);
 
 function reset() {
     resultArea.textContent = '즐거운 게임에 초대합니다 :)';
-    computerNum = pickRandomNum();
+    pickRandomNum();
     playButton.disabled = false;
     chance = 3;
-    chanceArea.textContent = '남은 찬스: 5번';
+    chanceArea.textContent = '남은 찬스: 3번';
     history = [];
     userInput.value = '';
 }
 
 function play() {
     let userValue = userInput.value;
+    console.log("userValue" + userValue)
+    console.log("computerNum" + computerNum)
     if (!validateNumber(userValue)) return;
 
     chance--;
@@ -58,20 +60,23 @@ function play() {
     }
 }
 
-const validateNumber = (input) => {
+function validateNumber(input) {
     if (!input || isNaN(input)) {
         resultArea.textContent = '⚠️ 숫자를 입력해주세요!';
+        userInput.value = '';
         return false;
     }
 
     const num = Number(input);
     if (num < 1 || num > 100) {
         resultArea.textContent = '⚠️ 1부터 100 사이의 숫자만 입력 가능합니다!';
+        userInput.value = '';
         return false;
     }
 
     if (history.includes(input)) {
         resultArea.textContent = '⚠️ 이미 입력한 숫자입니다!';
+        userInput.value = '';
         return false;
     }
     return true;
